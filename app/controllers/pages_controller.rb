47ef8e2b73a1523edaae25de7ed9a3803e72fa72
alban vervust <alban.vervust@gmail.com>
2011-05-04 22:27:06 +0200

    fini avec les premieres pages statics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   
   def home
+    @title = "Accueil"
   end
 
   def contact
+    @title = "Contactez-nous!"
+  end
+  
+  def about
+    @title = "A propos de nous"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 StatApp::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
